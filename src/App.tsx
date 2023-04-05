988350f50b2c0a7fce92717253cef95b28f28bcc
balsa-asanovic <balsaasanovic95@gmail.com>
2023-04-05 23:27:58 +0200

    removed useState from import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import './App.css';
 import Cube from './components/Cube/Cube';
 import Sphere from './components/Sphere/Sphere';
